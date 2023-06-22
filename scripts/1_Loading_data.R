ff3726d25a47ae5256a3d221428ccd041e4e6ac9
Irina Velez Lopez <ia.velez61@uniandes.edu.co>
2023-06-22 13:07:45 -0500

    Update 1_Loading_data.R

--- scripts/1_Loading_data.R
+++ scripts/1_Loading_data.R
@@ -1,46 +1,53 @@
 ##########################################################
-#       Problem Set 1. Predicting Income
+#         Problem Set 1. Predicting Income
 #           Big data and Machine Learning
-#             Author: Irina Vélez
-#           Universidad de los Andes
+#             Universidad de los Andes
 ##########################################################
+'''
+Authors:
 
-#### Initial Configuration ####
+- Daniel Casas Bautista
+- Lucia Fillippo
+- Miguel Angel Victoria Simbaqueva 
+- Irina Andrea Vélez López
+'''
+
+### Initial Configuration
+if(!require(pacman)) install.packages("pacman")
 require(pacman)
 p_load(tidyverse, rvest)
 
-## Web scraping
-data_url = "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
 
-data_html = read_html(data_url) ## leer el html de la página
-class(data_html) ## ver la clase del objeto
+### Loading data  
+data_url = "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_"
 
-db = data_html %>% html_table()
-length(my_table)
-
-
+# Loop
 db_list <- lapply(1:10, function(i) {
-  url <- paste0(data_url, "page", i, ".html")
-  page <- read_html(url)
-  
-  tables <- page %>% html_table()
-  tables
+  url <- paste0(data_url, "page_", i, ".html")
+  page <- read_html(url) 
+  tabla <- page %>% html_table
+  db_geih <- data.frame(tabla)
 })
 
-db <- do.call(rbind, db_list)
+db_geih <- do.call(rbind,db_list) # Concatenate the tables in a dataframe
+view(db_geih)
 
+### Filtering the data
+# Details of the variable age 
+summary(db_geih$age)
+sum(is.na(db_geih$age))
+class(db_geih$age)
 
-db_list [[1]]
-db <- do.call(rbind, db_list)
-View(db_list)
+db_geih_filtered <- db_geih[db_geih$age > 18, ]
+summary(db_geih_filtered$age)
 
-lapply(db_list, html_structure)
+## Dealing with missing values
+# Count missing values for each column and sorting
+missing_counts <- sapply(db_geih_filtered, function(x) sum(is.na(x)))
+sorted_missing <- sort(missing_counts, decreasing = TRUE)
+top_missing <- head(sorted_missing, 20)
 
+length(sorted_missing) # Correspond to the number of variables in the GEIH: 178
 
-
-db_list <- lapply(1:10, data_url, read_html)
-db_list [[1]]
-db_list [[2]]
-view(db_list)
-
-
+view(sorted_missing)  # Display the vector in decreasing order
+view(top_missing)     # Display the vector with the variables that have the most missing values
